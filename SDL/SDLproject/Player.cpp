87e65abb3ee2762992d8a0c1dfe957a13c69de98
mpx9411 <56627018+mpx9411@users.noreply.github.com>
2019-12-13 13:04:05 +0100

    Player functionality and images

--- SDL/SDLproject/Player.cpp
+++ SDL/SDLproject/Player.cpp
@@ -1,1 +1,17 @@
+
+
 #include "Player.h"
+
+Player::Player(int x, int y, int w, int h, string fLoc) :GameObject{x, y, w, h}{
+	char* image = new char[fLoc.length() + 1];
+	strcpy(image, fLoc.c_str);
+	SDL_Surface* pSprite = SDL_LoadBMP(image);
+	SDL_Texture* pTex = SDL_CreateTextureFromSurface(eng.getRen(), pSprite);
+
+}
+
+Player* Player::getInstance(int x, int y, int h, int w, string fLoc) {
+	return new Player(x, y, h, w, fLoc);
+}
+
+//SKRIV DRAW METOD
--- SDL/SDLproject/Player.h
+++ SDL/SDLproject/Player.h
@@ -1,11 +1,21 @@
-#pragma once
-#include "Sprite.h"
+#ifndef PLAYER.H
+#define PLAYER.H
+#include "GameObject.h"
+#include "Engine.h"
 #include "SDL.h"
-class Player :	public Sprite{
+#include <string>
+using namespace std;
+
+class Player :	public GameObject{
 public:
-	Player getInstance(int x);
+	Player* getInstance(int x, int y, int h, int w, string fLoc);
 	void draw() const;
 	void tick();
-
+	~Player();
+protected:
+	Player(int x, int y, int h, int w, string fLoc);
 };
+
+#endif
+
 
